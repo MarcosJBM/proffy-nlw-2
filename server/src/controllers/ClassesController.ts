5716034802ef643e808b279cc0332792c037a24f
MarcosJBM <marcosjbotene@gmail.com>
2020-08-08 21:49:35 -0300

    Criação da Listagem e Filtragem de Aulas.

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -10,6 +10,42 @@ interface ScheduleItem {
 };
 
 export default class ClassesController {
+    //Lista as aulas
+    async index(request: Request, response: Response) {
+        const filters = request.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        //Caso o usuário não preencha os filtros de Busca.
+        if (!filters.week_day || !filters.subject || !filters.time) {
+            return response.status(400).json({
+                error: 'Missing filters to searching classes'
+            });
+        };
+
+        //Converte os Horarios das aulas pra Minutos.
+        const timeInMinutes = convertHourToMinutes(time);
+
+        //Faz as filtragens.
+        const classes = await db('classes')
+            .whereExists(function () {
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
+
+        return response.json(classes);
+    };
+
+    //Cria uma nova aula.
     async create(request: Request, response: Response) {
         const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
