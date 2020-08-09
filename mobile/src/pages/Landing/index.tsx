c432902b532308ca2f9a2331a5ef82004f48fb05
MarcosJBM <marcosjbotene@gmail.com>
2020-08-09 19:37:05 -0300

    Estruturação e Estilização da Landing Page.

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
-import './styles.css';
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
 
-const Landing = () => {
+import styles from './styles';
+
+function Landing() {
     return (
-        <View>
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner} />
 
+            <Text style={styles.title}>
+                Seja Bem-Vindo, {'\n'}
+                <Text style={styles.titleBold}>O que deseja fazer?</Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon} />
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.totalConnections}>
+                Total de 285 conexões realizadas
+                <Image source={heartIcon} />
+            </Text>
         </View>
     );
 };
